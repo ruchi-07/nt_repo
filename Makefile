7321f95460aea6da153f69344855362694185b57
Naman Thaker <naman.thaker@einfochips.com>
2021-10-22 14:42:40 +0530

    Added main_3.c
    Also updated Makefile to build main_3.c
    
    Signed-off-by: Naman Thaker <naman.thaker@einfochips.com>

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 all:
 	gcc main.c -o main
 	gcc main_2.c -o main_2
+	gcc main_3.c -o main_3
 
 clean:
 	rm -rf main
 	rm -rf main_2
+	rm -rf main_3
---
+++ main_3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	char str[1000] = "Test String", rev[1000] = "\0";
+	int i, j, count = 0;
+	printf("String Before Reverse: %s", str);
+
+	//finding the length of the string
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	j = count - 1;
+
+	//reversing the string by swapping
+	for (i = 0; i < count; i++)
+	{
+		rev[i] = str[j];
+		j--;
+	}
+
+	printf("\nString After Reverse: %s\n", rev);
+}
+
