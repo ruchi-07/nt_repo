c7902fcfd6df87382d120036795bcb90d3468403
Naman Thaker <naman.thaker@einfochips.com>
2021-10-22 14:43:55 +0530

    Optimized main_3.c code
    Signed-off-by: Naman Thaker <naman.thaker@einfochips.com>

--- main_3.c
+++ main_3.c
@@ -4,22 +4,19 @@ int main()
 {
 	char str[1000] = "Test String", rev[1000] = "\0";
 	int i, j, count = 0;
+
 	printf("String Before Reverse: %s", str);
 
 	//finding the length of the string
-	while (str[count] != '\0')
-	{
-		count++;
-	}
+	while (str[count] != '\0') { count++; }
+
 	j = count - 1;
 
 	//reversing the string by swapping
-	for (i = 0; i < count; i++)
-	{
+	for (i = 0; i < count; i++) {
 		rev[i] = str[j];
 		j--;
 	}
 
 	printf("\nString After Reverse: %s\n", rev);
 }
-
